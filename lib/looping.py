830b38c56e8bc97fba9caed59c99840483ffe2a9
karis-m <moses.kamau@student.moringaschool.com>
2023-08-17 07:01:24 -0400

    complete assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    for i in int_list:
+        print(i*i)
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,100):
+        if (i%3 == 0 and i%5 == 0):
+            print("fizzbuzz")
+        elif i%3 == 0:
+            print("fizz")
+        elif i%5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+happy_new_year()
+square_integers([1, 2, 3, 4, 5, 6])
+fizzbuzz()
